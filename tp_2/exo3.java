49832b631894669b662fe5fa5adbe9f13fad7cce
Mohamed <mhmdhayza@gmail.com>
2025-03-11 22:52:41 +0100

    solution of exo 3 of tp 2

---
+++ tp_2/exo3.java
@@ -0,0 +1,27 @@
+package tp_2;
+
+public class exo3 {
+    public static void main(String[] arg){
+        // variables 
+        int a = 3, b = 5 ;
+        int x = 3, y = 5, z = 2, r = 6 ;
+        char lettre = 'i', car = 'j' ;
+
+        // logical stuff
+        System.out.println("is (a != b) : " + (a != b));
+        System.out.println("is (a + 2 == b) : " + (a + 2 == b));
+        System.out.println("is (a + 8 < 2 * b) : " + (a + 8 < 2 * b));
+        System.out.println("is (lettre <= car) : " + (lettre <= car));
+        System.out.println("is (lettre == 'w') : " + (lettre == 'w') );
+        System.out.println("is (x < y) && (z < r) : " + ( (x < y) && (z < r) ));
+        System.out.println("is (x > y) || (z < r) : " + ((x > y) || (z < r)));
+        System.out.println("is !(z < r) : " + !(z < r));
+
+
+
+
+
+
+
+    }
+}
